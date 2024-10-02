9cdd941c3529735bbdc1a9cc5dc196f6e0cd5aee
chaeyjs827 <chaeyjs827@gmail.com>
2024-10-02 23:55:41 +0900

    [멀티 모듈 분리 작업] - module-infra - module-domain - module-common - module-gifticon - module-user
    현재 빌드는 성공 상태.
    
    [TODO]
    1. module-gifticon, module-user Application 등록 및 실행
    2. DB 연동
    3. 각 모듈 별 통신은 FeignClient로 구현

--- module-gifticon/src/main/java/com/perfect/giftibox/gifticon/service/SaveGifticonService.java
+++ module-gifticon/src/main/java/com/perfect/giftibox/gifticon/service/SaveGifticonService.java
@@ -3,26 +3,24 @@ package com.perfect.giftibox.gifticon.service;
 import com.perfect.giftbox.domain.aggregate.GifticonAggregate;
 import com.perfect.giftbox.domain.vo.GifticonSaveCommand;
 import com.perfect.giftibox.gifticon.port.in.SaveGifticonUseCase;
-import com.perfect.gifticon.gifticon.domain.aggregate.GifticonAggregate;
-import com.perfect.gifticon.gifticon.domain.vo.GifticonSaveCommand;
-import com.perfect.gifticon.gifticon.port.in.SaveGifticonUseCase;
-import com.perfect.gifticon.users.adapter.out.persistance.UsersQueryRepository;
 
 public class SaveGifticonService implements SaveGifticonUseCase {
 
     private final GifticonAggregate gifticonCommandPort;
-    private final UserQuery usersQueryRepository;
 
-    public SaveGifticonService(GifticonAggregate gifticonCommandPort, UsersQueryRepository usersQueryRepository) {
+    public SaveGifticonService(GifticonAggregate gifticonCommandPort) {
         this.gifticonCommandPort = gifticonCommandPort;
-        this.usersQueryRepository = usersQueryRepository;
     }
 
     @Override
     public void registerGifticon(GifticonSaveCommand gifticonSaveCommand) {
 //        1. [todo]유효 회원 확인(Users 인데, 이건 아마 interceptor에서 처리할 듯)
 //        UsersEntity usersEntity = usersQueryRepository.getUsers(1L);
-        usersQueryRepository.getUsers(gifticonSaveCommand.usersId());
+
+
+//        usersQueryRepository.getUsers(gifticonSaveCommand.usersId());
+
+
 //        2. 해당 회원의 등록 가능 기프티콘 수 확인(Users 도메인)
 //        3. 기프티콘 등록(Gifticon 도메인)
 //        4. 해당 회원 기프티콘 등록 수 갱신(Users 도메인)
